4881b518fe0cc55907577afc3aab1dc821349ce4
ARturMZ <voxi7713@gmail.com>
2025-04-14 13:07:45 +0300

    upl posts to main page & post's img full screen

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -79,9 +79,13 @@ export const PostHero: React.FC<{
       </div>
       <div className="min-h-[80vh] select-none">
         {metaImage && typeof metaImage !== 'string' && (
-          <Media fill imgClassName="-z-10 object-cover" resource={metaImage} />
-        )}
-        <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent" />
+          <Media
+          fill
+          imgClassName="absolute top-0 left-0 w-full h-full object-cover -z-10"
+          resource={metaImage}
+        />
+      )}
+      <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent" />
       </div>
     </div>
   )
